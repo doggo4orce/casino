963b7dd8010f9aa8175c9150b272e78510a9a2f8
Roo Biki <doggo4orce@gmail.com>
2023-10-08 07:49:25 -0700

    added a little testing for chcharacters and entities

--- test_pc.py
+++ test_pc.py
@@ -5,21 +5,29 @@ import unittest
 
 class TestPC(unittest.TestCase):
 
-  def test_entity(self):
+  def test_10_entity_aaa(self):
+    print("test_entity")
     ent = structs.entity_data()
     ent.name = "a young dog"
     ent.namelist = ["dog", "young"]
     ent.ldesc = "A young dog chases after a ball."
     ent.desc = "<p>It looks like a young puppy.</p>"
-
+    print("\n---\n", ent.debug(), "---\n")
     self.assertTrue(ent.has_alias("young"))
     self.assertTrue(ent.has_alias("dog"))
     self.assertFalse(ent.has_alias("cat"))
     self.assertEqual(ent.Name, "A young dog")
-    self.assertEqual(ent.room, structs.unique_identifier.from_string(config.VOID_ROOM))
+    self.assertEqual(ent.room, None)
+
+    print(vars(ent))
     
-  def test_character(self):
+  def test_20_character(self):
+    print("test_character")
     char = pc.character()
+
+    print("\n---\n", char.debug(), "---\n")
+
+    print(vars(char))
 
   def test_npc(self):
     pass

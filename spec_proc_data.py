0df6713e8f2208d2624c32b229bebbe81746b82e
Kyle Schlitt <kschlitt@tru.ca>
2023-10-19 16:16:42 -0700

    tweaked spec_proc testing

--- spec_proc_data.py
+++ spec_proc_data.py
@@ -4,13 +4,39 @@ import string_handling
 # TODO (idea?): add 'behaviour' field to npc/npc_protos to manage the lists of spec_procs
 class spec_proc_data:
   expected_args = []
-  """name = representation as a string
-     func = behaviour function, uses return value as message to nanny
-     args = list of args that func is expecting
-     num_args = number of args that func is expecting"""
+
+  """name      = representation as a string
+     func      = behaviour function, uses return value as message to nanny
+     args      = list of args that func is expecting
+     num_args  = number of args that func is expecting
+     arg_error = checks whether self.func accepts the correct parameters"""
+
   def __init__(self, name, func):
     self.name = name
     self.func = fn
+
+  @property
+  def name(self):
+    return self._name
+  @property
+  def func(self):
+    return self._func
+  @property
+  def args(self):
+    return inspect.getfullargspec(self.func).args
+  @property
+  def num_args(self):
+    return len(self.args)
+  @property
+  def arg_error(self):
+    return self.arg_error_brief() != None
+
+  @name.setter
+  def name(self, new_name):
+    self._name = new_name
+  @func.setter
+  def func(self, new_func):
+    self._func = new_func
 
   """arg_error_brief()  <-- first disagreement between self.args and self.expected_args
      arg_error_full()   <-- same as above but with more context
@@ -35,23 +61,3 @@ class spec_proc_data:
 
   def correct_fn_args(self):
     return self.first_fn_arg_error_brief() == None
-
-  @property
-  def name(self):
-    return self._name
-  @property
-  def func(self):
-    return self._func
-  @property
-  def args(self):
-    return inspect.getfullargspec(self.func).args
-  @property
-  def num_args(self):
-    return len(self.args)
-
-  @name.setter
-  def name(self, new_name):
-    self._name = new_name
-  @func.setter
-  def func(self, new_func):
-    self._func = new_func

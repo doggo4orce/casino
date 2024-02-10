08a6b76a654b9e6c12aca5e315126de78814f812
Roo Biki <doggo4orce@gmail.com>
2024-02-09 21:20:43 -0800

    deleted old versions of display/clean_up

--- buffer.py
+++ buffer.py
@@ -26,9 +26,11 @@ class buffer:
 
   def __setitem__(self, key, value):
     self._contents[key] = value
+
   """Attributes:
      num_lines = returns line count
      is_empty = check if line count is zero"""
+
   @property
   def num_lines(self):
     return len(self._contents)
@@ -137,103 +139,6 @@ class buffer:
 
     return buffer(clean)    
 
-  def clean_up2(self):
-    ret_val = buffer() # store the return value
-    line_buf = ""      # store progress while building line
-    par_buf = ""       # store progress while building paragraph
-
-    for idx, line in self._contents:
-      if not p_open:
-        pattern = re.compile(OPEN_PARAGRAPH)
-        match = re.search(pattern, line)
-
-        if match == None:
-          ret_val.add_line(line)
-          continue
-
-        j = match.span()[0] # beginning of match
-        k = match.span()[1] # beginning of suffix
-
-        # line[:j] <-- what we will add to ret_val if </p> isn't also in this line
-        # line[k:] <-- what we will set p_buf to if it doesn't contain </p>
-
-    return None
-
-  def clean_up3(self):
-    ret_val = buffer()
-    p_buffer = ""
-    p_open = False
-
-    # go through one line at a time
-    for line in self._contents:
-
-      # ignore empty lines within a paragraph
-      if p_open and line == "":
-        continue
-
-      while (True):
-        # if we're in the middle of a paragraph, find out if it closes on this line
-        if p_open:
-          target = CLOSE_PARAGRAPH
-        # if it doesn't, then find out if a paragraph opens on this line
-        else:
-          target = OPEN_PARAGRAPH
-
-        # see if we find out target
-        pattern = re.compile(target)
-        match = re.search(pattern, line)
-
-        # if not, then just carry on with whatever we're doing
-        if match == None:
-          # whether we are in the middle of a paragraph
-          if p_open:
-            p_buffer += line + " "
-          # or recording verbatim text
-          else:
-            ret_val.add_line(line)
-          break
-
-        # TODO: peek ahead to make sure a CLOSE_PARAGRAPH is coming
-        # otherwise, the OPEN_PARAGRAPH we just found should not be
-        # touched. Feb 9, 7:47
-
-        # but if we did find a match, start splicing
-        else:
-          x = match.span()[0]
-          y = match.span()[1]
-
-          # if our paragraph is open, then we found the closing tag
-          if p_open:
-            # so close the paragraph
-            p_open = False
-
-            # add the pre-tag string into the paragraph buffer
-            p_buffer += line[:x]
-
-            # now record the paragraph as a single line
-            ret_val.add_line(f"{OPEN_PARAGRAPH}{p_buffer}{CLOSE_PARAGRAPH}")
-
-            # and reset the buffer for the next one
-            p_buffer = ""
-
-          # otherwise we found an opening paragraph tag
-          else:
-            # so open the paragraph
-            p_open = True
-
-            # unless its the beginning of the line
-            if x != 0:
-              # add the pre-tag string as a verbatim line
-              ret_val.add_line(line[:x])
-
-          # get the post-tag string ready to be handled on the next iteration
-          line = line[x + len(target):]
-
-        if line == "":
-          break
-
-    return ret_val
-
   def display(self, width, indent=True, numbers=False, color=True):
     temp_buf = self.make_copy()
     final_buf = buffer()
@@ -254,33 +159,6 @@ class buffer:
       ret_val = string_handling.proc_color(line)
 
     return ret_val
-
-  # def display2(self, width, format=True, indent=True, numbers=False, color=True):
-  #   ret_val = ""
-  #   buf = self.make_copy()
-
-  #   if format:
-  #     buf = buf.clean_up()
-
-  #   for idx, line in enumerate(buf._contents):
-
-  #     if format and (line[:len(OPEN_PARAGRAPH)] == OPEN_PARAGRAPH and line[(-1)*len(CLOSE_PARAGRAPH):] == CLOSE_PARAGRAPH):
-  #       line = line[len(OPEN_PARAGRAPH):]
-  #       line = line[:(-1)*len(CLOSE_PARAGRAPH)]
-  #       line = string_handling.paragraph(line, width, indent)
-
-  #     if color:
-  #       line = string_handling.proc_color(line)
-
-  #     if numbers:
-  #       line = f"L{idx + 1}: " + line
-      
-  #     ret_val += line
-
-  #     if idx != len(buf._contents) - 1:
-  #       ret_val += "\r\n"
-
-  #   return ret_val
 
   # TODO:  adjust this to use '\r\n'.join(self._contents)'
   def str(self, numbers=False):

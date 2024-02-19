8344c92434defcb514276748a19ca84cab6f62e6
Roo Biki <doggo4orce@gmail.com>
2024-02-19 08:35:48 -0800

    separated testing for entity protos into test_entity_data.py

--- test_entity_data.py
+++ test_entity_data.py
@@ -1,4 +1,5 @@
 import entity_data
+import entity_proto_data
 import unittest
 
 class TestEntity(unittest.TestCase):
@@ -17,21 +18,6 @@ class TestEntity(unittest.TestCase):
     self.assertEqual(ent.Name, "A young dog")
     self.assertEqual(ent.ldesc, "A young dog chases after a ball.")
     self.assertEqual(ent.desc, "<p>It looks like a young puppy.</p>")
-    self.assertEqual(ent.room, None)
-
-  def test_proto(self):
-    proto = entity_data.entity_proto_data()
-    proto.name="a goblin"
-    proto.namelist=["goblin"]
-    proto.ldesc="A goblin stands here, smiling mischievously."
-    proto.desc = "<p>It looks like it has something up its sleeve.</p>"
-
-    ent = entity_data.entity_data(proto)
-    self.assertEqual(ent.name, "a goblin")
-    self.assertEqual(ent.namelist, ["goblin"])
-    self.assertEqual(ent.Name, "A goblin")
-    self.assertEqual(ent.ldesc, "A goblin stands here, smiling mischievously.")
-    self.assertEqual(ent.desc, "<p>It looks like it has something up its sleeve.</p>")
     self.assertEqual(ent.room, None)
 
 if __name__ == "__main__":

f2d37f563f36f11abeb4a1f58f7fdefd911e2958
Roo Biki <doggo4orce@gmail.com>
2024-09-28 10:44:25 -0700

    created files for redit sand medit save data for when they are added later

---
+++ medit_save_data.py
@@ -0,0 +1,4 @@
+@dataclasses.dataclass
+class medit_save_data:
+  """ fill this in when medit is developed """
+  pass
---
+++ oedit_save_data.py
@@ -0,0 +1,4 @@
+@dataclasses.dataclass
+class oedit_save_data:
+  """ fill this in when oedit is developed """
+  pass

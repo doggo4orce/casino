f72b7687165580ecebfee754b0df9a6ee939ee5d
Roo Biki <doggo4orce@gmail.com>
2024-10-01 07:22:56 -0700

    organized npc_proto_data and obj_proto_data and realized they are full of wrappers, yet still need more wrappers, and are essentially the same class.  it would be better to factor all this common code out.  better yet, why not just derive them both from entity_proto and then the only thing they'll have to wrap is the unique_id_data stuff

--- npc_proto_data.py
+++ npc_proto_data.py
@@ -10,14 +10,12 @@ import unique_id_data
 class npc_proto_data:
   """Each of these fields are loaded into the corresponding NPC fields.
      entity_proto = name, namelist, ldesc, desc (entity_data.py)
-     unique_id = global and local identifier (unique_id.py)
-     behaviour = spec proc manager (behaviour_data.py)"""
+     unique_id = unique identifier of NPC prototype"""
   def __init__(self):
     self.entity_proto = entity_proto_data.entity_proto_data()
     self.unique_id = unique_id_data.unique_id_data()
-    self.behaviour = behaviour_data.behaviour_data()
 
-    # wrappd to entity_proto
+    # wrapped to entity_proto
     self.ldesc = "An unfinished NPC prototype stands here."
 
   @property
@@ -27,8 +25,18 @@ class npc_proto_data:
   def unique_id(self):
     return self._unique_id
   @property
+  def ldesc(self):
+    return self.entity_proto.ldesc
+
+  @property
+  def id(self):
+    return self.unique_id.id
+  @property
+  def zone_id(self):
+    return self.unique_id.zone_id
+  @property
   def behaviour(self):
-    return self._behaviour
+    return self.entity_proto._behaviour
 
   @entity_proto.setter
   def entity_proto(self, new_entity_proto):
@@ -36,41 +44,56 @@ class npc_proto_data:
   @unique_id.setter
   def unique_id(self, new_uid):
     self._unique_id = new_uid
-  @behaviour.setter
-  def behaviour(self, new_behaviour):
-    self._behaviour = new_behaviour
-
-  # Wrapped properties
-  @property
-  def ldesc(self):
-    return self.entity_proto.ldesc
-  @property
-  def id(self):
-    return self.unique_id.id
-  @property
-  def zone_id(self):
-    return self.unique_id.zone_id
-
-  # Wrapped setters
   @ldesc.setter
   def ldesc(self, new_ldesc):
     self.entity_proto.ldesc = new_ldesc
+
   @id.setter
   def id(self, new_id):
     self.unique_id.id = new_id
   @zone_id.setter
   def zone_id(self, new_zid):
     self.unique_id.zone_id = new_zid
+  @behaviour.setter
+  def behaviour(self, new_behaviour):
+    self.entity_proto.behaviour = new_behaviour
 
-  """assign_spec_proc(spec_proc)   <- assign a single spec proc to behaviour
+  """Wrapped to entity_proto:
+
+     add_alias(alias)       <- adds a new alias to entity_proto
+     has_alias(alias)       <- check entity_proto for alias
+     num_aliases            <- count aliases in entity_proto
+     remove_aliases(alias)  <- remove alias from entity_proto
+     remove_all_alias()     <- remove all aliases"""
+
+  def add_alias(self, alias):
+    self.entity_proto.add_alias(alias)
+      
+  def has_alias(self, alias):
+    return self.entity_proto.has_alias(alias)
+
+  @property
+  def num_aliases(self):
+    return self.entity_proto.num_aliases
+
+  def remove_alias(self, alias):
+    self.entity_proto.remove_alias(alias)
+
+  def remove_all_alias(self):
+    # this seems lazy but why not?
+    self.entity_proto.remove_all_aliases()
+
+"""Wrapped to entity_proto:
+
+     assign_spec_proc(spec_proc)   <- assign a single spec proc to behaviour
      assign_spec_procs(spec_procs) <- assign a list of spec procs to behaviour"""
 
   def assign_spec_proc(self, spec_proc):
-    self.behaviour.assign(spec_proc)
+    self.entity_proto.behaviour.assign(spec_proc)
 
   def assign_spec_procs(self, spec_procs):
     for spec_proc in spec_procs:
-      self.behaviour.assign(spec_proc)
+      self.entity_proto.behaviour.assign(spec_proc)
 
   def __str__(self):
     ret_val = f"NPC: {CYAN}{self.entity_proto.name}{NORMAL}\r\n"

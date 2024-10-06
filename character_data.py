12ed200c328ac0afa38fd7dc3ecd7816130f190c
Roo Biki <doggo4orce@gmail.com>
2024-10-06 13:37:48 -0700

    character derived from entity rather than composed from it

--- character_data.py
+++ character_data.py
@@ -1,62 +1,69 @@
 import config
-import editor
+import copy
 import entity_data
-import inventory
+import inventory_data
 import mudlog
 import unique_id_data
 
 class character_data(entity_data.entity_data):
   """Creates a new character which can act within the world.
 
-    name      = characters name (wrapped from entity)
-    Name      = characters name capitalized (wrapped from entity)
-    desc      = characters description as a buffer (wrapped from entity)
-    ldesc      = how character is listed in a room to others (wrapped from entity)
-    room      = what room character is in (wrapped from entity)
-    namelist  = list of aliases for character to be accessed by (wrapped from entity)
-    inventory = iterable container consisting of all objects held (see object.py)
-    in_zone   = return zone_id of character's location (wrapped from entity)
+    inventory = all objects held (private)"""
 
-    Note: This class is meant to encapsulate the functionality shared by pcs/npcs,
-    so that they need not be distinguished between throughout this codebase.  While
-    there is nothing stopping one from instantiating it directly, such use is not intended."""
   def __init__(self):
     super().__init__()
     self.name = "an unfinished character"
     self.desc = "This character looks unfinished."
     self.ldesc = "An unfinished character is here."
     self.room = unique_id_data.unique_id_data.from_string(config.VOID_ROOM)
-    self.reset_alias("unfinished", "character")
-    self.inventory = inventory.inventory()
+    self.reset_aliases("unfinished", "character")
+    self._inventory = inventory_data.inventory_data()
 
-  # Getters
-  @property
+  """copy_from(ch)          <- make a copy, ignores inventory
+     give_object(obj)       <- add object to inventory
+     lose_object(obj)       <- remove object from inventory
+     has_object(obj)        <- check if object is in inventory
+     transfer_obj(obj, ch)  <- give obj to ch
+     transfer_inventory(ch) <- give all inventory to ch
+     inventory()            <- copy of self._inventory as list
+     write(msg)             <- no operation, should be over-written by derived classes
+     debug()                <- returns debugging information as a string"""
+
+  def copy_from(self, ch):
+    super().copy_from(ch)
+    # copy character specific fields below
+
+  def give_object(self, obj):
+    if not self.has_object(obj):
+      self._inventory.add_object(obj)
+    else:
+      mudlog.error(f"Trying to give {obj} to character {self} who already has it!")
+
+  def lose_object(self, obj):
+    if self.has_object(obj):
+      self._inventory.remove_object(obj)
+    else:
+      mudlog.error(f"Character {self} trying to lose {obj} but doesn't have it!")
+ 
+  def has_object(self, obj):
+    return self._inventory.has_object(obj)
+
+  def transfer_obj(self, obj, ch):
+    self._inventory.transfer_obj(obj, ch._inventory)
+
+  def transfer_inventory(self, ch):
+    self._inventory.transfer_all(ch._inventory)
+
   def inventory(self):
-    return self.inventory
-  @property
-  def in_zone(self):
-    return self.entity.in_zone
-
-  # Setters
-  @inventory.setter
-  def inventory(self, new_inventory):
-    self._inventory = new_inventory
-
-  """write(message)   <-- no operation, should be over-written by derived classes
-     in_zone()        <-- return zone_id of character's location
-     has_alias(alias) <-- check if alias is in self.namelist (wrapped from entity)
-     debug()          <-- display state in readable string"""
+    return self._inventory.contents()
 
   def write(self, message):
-    logging.warning(f"Attempting to send message {message} to character {self.entity.name}")
+    mudlog.warning(f"Character {self.name}'s write function called with \"{message}\"")
     return
 
-  def has_alias(self, alias):
-    return self._entity.has_alias(alias)
-
   def debug(self):
-    ret_val = self.entity.debug()
-    ret_val += f"Type: {type(self)}\r\n"
+    ret_val = f"Character Information\r\n"
+    ret_val += super().debug()
     return ret_val
 
   def __str__(self):
--- test_character_data.py
+++ test_character_data.py
@@ -1,22 +1,23 @@
 import character_data
-import editor
+import object_data
 import entity_data
-import unittest
 import unique_id_data
+
+import unittest
 
 class TestCharacter(unittest.TestCase):
   def test_character(self):
     char = character_data.character_data()
 
     char.name = "bob"
-    char.desc = editor.buffer("<p>This is Bob.  Bob is nice.  Be like Bob.</p>")
+    char.desc = "<p>This is Bob.</p>"
     char.ldesc = "Bob is standing around."
     char.room = unique_id_data.unique_id_data("stockville", "casino")
-    char.namelist = ["bob", "nice"]
+    char.reset_aliases("bob", "nice")
 
     self.assertEqual(char.name, "bob")
     self.assertEqual(char.Name, "Bob")
-    self.assertEqual(type(char.desc), editor.buffer)
+    self.assertEqual(char.desc, "<p>This is Bob.</p>")
     self.assertEqual(char.ldesc, "Bob is standing around.")
     self.assertEqual(str(char.room), "stockville[casino]")
     self.assertEqual(char.room.zone_id, "stockville")
@@ -24,31 +25,75 @@ class TestCharacter(unittest.TestCase):
     self.assertEqual(char.in_zone, "stockville")
     self.assertTrue(char.has_alias("bob"))
     self.assertTrue(char.has_alias("nice"))
-
-    # I'm essentially testing the @entity.setter here
-    # does this go outside the scope of a unit test?
-    entity = entity_data.entity_data()
-    entity.name = "terry"
-    entity.namelist = ["terry", "mean"]
-    entity.ldesc = "Terry is standing here."
-    entity.desc = editor.buffer("<p>This is Terry.  Terry is mean.  Don't be like Terry.</p>")
-    entity.room = unique_id_data.unique_id_data("newbie_zone", "hallway")
-    char.entity = entity
-
-    # assuming entity getters are correct, tested in test_entity_data.py
-    self.assertEqual(char.name, entity.name)
-    self.assertEqual(char.Name, entity.Name)
-    self.assertEqual(char.ldesc, entity.ldesc)
-    self.assertEqual(char.desc, entity.desc)
-    self.assertEqual(char.room, entity.room)
+    self.assertFalse(char.has_alias("notmyname"))
 
     char.write("This should trigger a warning, but not raise an exception.")
-
-    self.assertTrue(char.has_alias("terry"))
-    self.assertTrue(char.has_alias("mean"))
 
     self.assertEqual(str(char), char.name)
 
     print(char.debug())
+
+  def test_inventory(self):
+    char = character_data.character_data()
+    obj1 = object_data.object_data()
+    obj2 = object_data.object_data()
+    obj3 = object_data.object_data()
+
+    char.give_object(obj1)
+    char.give_object(obj2)
+
+    self.assertTrue(char.has_object(obj1))
+    self.assertTrue(char.has_object(obj2))
+
+    self.assertFalse(char.has_object(obj3))
+
+    char.give_object(obj3)
+
+    self.assertEqual(char.inventory(), [obj1, obj2, obj3])
+
+    char.lose_object(obj1)
+
+    self.assertFalse(char.has_object(obj1))
+    self.assertEqual(char.inventory(), [obj2, obj3])
+
+  def test_copy_transfer(self):
+    char = character_data.character_data()
+    char.name = "bob"
+    char.desc = "<p>This is Bob.</p>"
+    char.ldesc = "Bob is standing around."
+    char.room = unique_id_data.unique_id_data("stockville", "casino")
+    char.reset_aliases("bob", "nice")
+
+    obj1 = object_data.object_data()
+    obj2 = object_data.object_data()
+
+    char.give_object(obj1)
+    char.give_object(obj2)
+
+    char2 = character_data.character_data()
+
+
+    char2.copy_from(char)
+
+    self.assertEqual(char.name, char2.name)
+    self.assertEqual(char.desc, char2.desc)
+    self.assertEqual(char.ldesc, char2.ldesc)
+
+    char.transfer_obj(obj1, char2)
+
+    self.assertFalse(char.has_object(obj1))
+    self.assertTrue(char2.has_object(obj1))
+
+    char.transfer_obj(obj2, char2)
+
+    self.assertFalse(char.has_object(obj2))
+    self.assertTrue(char2.has_object(obj2))
+
+    char2.transfer_inventory(char)
+    self.assertFalse(char2.has_object(obj1))
+    self.assertFalse(char2.has_object(obj2))
+    self.assertTrue(char.has_object(obj1))
+    self.assertTrue(char.has_object(obj2))
+    
 if __name__ == "__main__":
   unittest.main()

74777345478177d7485af225d0354bca7bf23d2d
kyleschlitt <schlitt@ualberta.ca>
2025-10-23 08:15:52 -0700

    a little more

--- telnet.py
+++ telnet.py
@@ -212,6 +212,7 @@ class tel_msg:
       if len(self.payload) < 4:
         return ret_val + " ".join([str(b) for b in self.payload]) + " " + incomplete
 
+      # express terminal dimenions as width
       ret_val += f"{self.payload[0]*256 + self.payload[1]}x{self.payload[2]*256 + self.payload[3]}"
     else:
       # we either don't recognize self.opt or don't have a special way to handle it
